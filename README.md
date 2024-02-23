881e350c2755087abde6a622ffe7736af9cec6d6
RInkalshah93 <161082680+RInkalshah93@users.noreply.github.com>
2024-02-24 02:24:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# session5-assignment
-session5 assignment
+# Session 5 Assignment MNIST Digit Recognition Rinkal Shah
+
+## Description
+This code imports the data from the MNIST digital library, performs convolution, and recognizes numbers.
+
+## Files
+
+1. **model.py**: Contains the definition of the CNN model.
+2. **utils.py**: Includes utility functions for loading and preprocessing the MNIST dataset.
+3. **S5.ipynb**: Notebook for training the model and saving the trained model weights.
+4. **README.md**: This file, providing an overview of the repository and instructions for reading the code.
---
+++ utils.py
@@ -0,0 +1,25 @@
+import torch
+from torchvision import datasets, transforms
+
+def get_data_loaders(batch_size=512):
+    train_transforms = transforms.Compose([
+        transforms.RandomApply([transforms.CenterCrop(22)], p=0.1),
+        transforms.Resize((28, 28)),
+        transforms.RandomRotation((-15., 15.), fill=0),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+    test_data = datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
+
+    kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}
+    train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_data, **kwargs)
+
+    return train_loader, test_loader
