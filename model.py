db1d16eaae8d14c60495d6d3c72998214cc8e3a0
RInkalshah93 <161082680+RInkalshah93@users.noreply.github.com>
2024-02-24 04:12:50 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,5 +1,28 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+train_transforms = transforms.Compose([
+    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),#applying random tranforamtion to given image in this case it is applying center crop with the probeblity of 0.1
+    transforms.Resize((28, 28)),#to resize the input image in this case we are changing the input image to 28x28
+    transforms.RandomRotation((-15., 15.), fill=0),#this is for randomly rotate the image between -15 to 15 degree and no padding added
+    transforms.ToTensor(),#this is to convert any PIL image to tensor formate
+    transforms.Normalize((0.1307,), (0.3081,)),#this to normalize the Tensor data for easy data  training
+    ])
+
+# Test data transformations
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),#this is to convert any PIL image to tensor formate
+    transforms.Normalize((0.1307,), (0.3081,))#this to normalize the Tensor data for easy data  testing.
+    ])
+
+batch_size = 512 # defined Batch size for processing image in one go
+
+kwargs = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 2, 'pin_memory': True}# created dictionary where shuffle of image is allow and specified subprocess to be done using num_workers and to improve the perfomance we used pin memory to tranform the data from cpu to gpu
+
+test_loader = torch.utils.data.DataLoader(test_data, **kwargs)#  to conver dictionary to keyword argument
+train_loader = torch.utils.data.DataLoader(train_data, **kwargs)#  to conver dictionary to keyword argument
 
 class Net(nn.Module):
     def __init__(self):
